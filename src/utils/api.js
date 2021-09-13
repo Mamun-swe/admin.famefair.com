75b5dbda63df4576e83b3b65ad6647ec2cddfb44
Mamun-swe <mhmamun166009@gmail.com>
2021-09-13 21:51:39 +0600

    API added

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
-export const api = "http://api.famefair.com.bd/api/v1/admin/";
-export const authApi = "http://api.famefair.com.bd/api/v1/auth/admin/";
-export const aclApi = "http://api.famefair.com.bd/api/v1/acl/";
+export const api = "https://api.famefair.com.bd/api/v1/admin/";
+export const authApi = "https://api.famefair.com.bd/api/v1/auth/admin/";
+export const aclApi = "https://api.famefair.com.bd/api/v1/acl/";
 // export const api = "https://api.eazybest.com/api/v1/admin/";
 // export const api = "https://fakestoreapi.com/";
